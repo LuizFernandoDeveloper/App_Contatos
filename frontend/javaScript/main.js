1caf5d006c04f33e73096245b13ea3e7ff76fe31
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2022-11-10 13:03:20 -0300

    update

--- frontend/javaScript/main.js
+++ frontend/javaScript/main.js
@@ -1,13 +1,30 @@
 const form = document.getElementById('form_contatos');
- alert("ssadawdaw")
+let linhas = ' <h1>Contatos Salvos</h1>';
 form.addEventListener('submit', function(e){
 
     e.preventDefault();
 
-    const personsName = getElementById('personsName');
-    const phoneNumber = getElementById('phoneNumber');
+    const personsName = document.getElementById('personsName');
+    const phoneNumber = document.getElementById('phoneNumber');
 
-    alert(`Nome da pessoa:${personsName}`);
-    alert(`Numero da pessoa:${phoneNumber}`);
+    const corpoLista = document.querySelector('article');
+
+    
+
+    let linha  = '<div class="contato">';
+    linha += '<div class="img-contatos">';
+    linha += ' <img src="./imagens/icons/Logo-Barra-Contatos3.svg" alt="logo contatos salvos">';
+    linha += '</div>';
+    linha += `<h2>Nome do contato:<span>${personsName.value}</span></h2>`;
+    linha += `<h2>Numero do Contato:<span>${phoneNumber.value}</span></h2>`;
+    linha += '</div>';
+
+    linhas += linha;
+
+    corpoLista.innerHTML = linhas ;
+
+    personsName.value = '';
+    phoneNumber.value = '';
 
 });
+
